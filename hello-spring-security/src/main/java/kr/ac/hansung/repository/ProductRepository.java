10bace1cef229f8c5c27997014848def1b36d434
choiwonjae <chldnjswo1234@naver.com>
2026-06-03 13:55:07 +0900

    [feat]학번 이름 추가, 초기 세팅 완료

---
+++ hello-spring-security/src/main/java/kr/ac/hansung/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package kr.ac.hansung.repository;
+
+import kr.ac.hansung.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    long countByStockEquals(int stock);
+
+    boolean existsByName(String name);
+}
